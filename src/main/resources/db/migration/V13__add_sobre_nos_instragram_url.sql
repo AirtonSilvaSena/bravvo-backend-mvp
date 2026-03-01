34455195a23d9106141407096aa75497be8cd50c
Airton Silva  Sena <airtonsilvasenaa@gmail.com>
2026-03-01 17:54:28 -0300

    att correção migration

--- src/main/resources/db/migration/V13__add_sobre_nos_instragram_url.sql
+++ src/main/resources/db/migration/V13__add_sobre_nos_instragram_url.sql
@@ -3,4 +3,4 @@
 
 ALTER TABLE estabelecimentos
 ADD COLUMN sobre_nos TEXT NULL COMMENT 'Texto público de apresentação do estabelecimento (sobre nós)',
-ADD COLUMN instagram_url VARCHAR(255) NULL COMMENT 'URL pública do Instagram',
+ADD COLUMN instagram_url VARCHAR(255) NULL COMMENT 'URL pública do Instagram';
