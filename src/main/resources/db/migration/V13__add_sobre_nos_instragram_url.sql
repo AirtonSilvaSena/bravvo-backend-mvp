eae61a50dcf72f6307ffa214bcf5ab429b406c01
Airton Silva  Sena <airtonsilvasenaa@gmail.com>
2026-03-01 18:08:53 -0300

    att migration v13

--- src/main/resources/db/migration/V13__add_sobre_nos_instragram_url.sql
+++ src/main/resources/db/migration/V13__add_sobre_nos_instragram_url.sql
@@ -1,5 +1,3 @@
--- V14__add_instagram_facebook_and_sobre_nos_to_estabelecimentos.sql
--- Adiciona colunas públicas de redes sociais e descrição institucional
 
 ALTER TABLE estabelecimentos
 ADD COLUMN sobre_nos TEXT NULL COMMENT 'Texto público de apresentação do estabelecimento (sobre nós)',
